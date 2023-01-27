a44eb96a12b3ec20a7fc0544ab7e02ae84948dee
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2023-01-27 23:32:57 +0300

    Creating Services,Repository and Controller Class

---
+++ src/main/java/dev/kerim/movies/MovieService.java
@@ -0,0 +1,13 @@
+package dev.kerim.movies;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+
+    }
+}
